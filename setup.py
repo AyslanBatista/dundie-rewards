2f7bb5dea0e4e4a90ebe07f541e2124e4985d390
ayslan batista <ayslan.batista@hotmail.com>
2022-08-27 16:10:03 -0300

    Set up project initial organization and metadata
    - Add setup.py to turn installable
    - Add __main__ for executable
    - Add __init__ for package
    
    closes #8

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setuptools | pyproject | external build tools (potry, flit)
+# Transformar um programa em instalável | pip install -e . | python setup.py build
+from setuptools import find_packages, setup
+
+setup(
+    name="dundie",
+    version="0.1.0", # x.y.z.
+    description="Reward Point System for Dunder Mifflin",
+    author="Ayslan Batista",
+    packages=find_packages(), #todas as pasta que contem __init__
+)
