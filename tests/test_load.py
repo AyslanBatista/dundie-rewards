ccb81d20c7fb1c96471f9d6f66f2bb343649fdfa
ayslan batista <ayslan.batista@hotmail.com>
2022-08-28 11:16:32 -0300

    Making an error

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,6 +1,8 @@
 # export PYTHONBREAKPOINT=ipdb.set_trace
 # breakpoin()
 # pytest -vv -s
+# pytest -s -m "unit" --junitxml=/tmp/result.xml
+
 import pytest
 
 from dundie.core import load
@@ -10,7 +12,13 @@ from .constants import PEOPLE_FILE
 
 @pytest.mark.unit
 @pytest.mark.high
-def test_load():
+def test_load_positive_has_2_people(request):
     """Test load function"""
     assert len(load(PEOPLE_FILE)) == 2
+    
+    
+@pytest.mark.unit
+@pytest.mark.high
+def test_load_positive_has_first_name_starts_with_j(request):
+    """Test load function"""
     assert load(PEOPLE_FILE)[0][0] == "J"
