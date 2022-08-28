ed0f7251a07a674b2d8c4542ad5b1675e1aadf55
ayslan batista <ayslan.batista@hotmail.com>
2022-08-28 10:49:45 -0300

    Adding finalizers, setup and teardown to tests

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,6 +1,9 @@
 # export PYTHONBREAKPOINT=ipdb.set_trace
 # breakpoin()
 # pytest -vv -s
+import os
+import uuid
+
 import pytest
 
 from dundie.core import load
@@ -8,9 +11,43 @@ from dundie.core import load
 from .constants import PEOPLE_FILE
 
 
+def setup_module():
+    print("roda antes dos testes desse modulo")
+
+
+def teardown_module():
+    print("rodar apos os testes desse modulo")
+
+
+@pytest.fixture(scope="function", autouse=True)
+def create_new_file(tmpdir):
+    file_ = tmpdir.join("new_file.txt")
+    file_.write("isso é sujeira...")
+    yield
+    file_.remove()
+
+
 @pytest.mark.unit
 @pytest.mark.high
-def test_load():
+def test_load(request):
     """Test load function"""
+    filepath = f"arquivo_indesejado-{uuid.uuid4()}.txt"
+    request.addfinalizer(lambda: os.unlink(filepath))
+
+    with open(filepath, "w") as file_:
+        file_.write("dados uteis somente para o teste")
+
+    assert len(load(PEOPLE_FILE)) == 2
+    assert load(PEOPLE_FILE)[0][0] == "J"
+
+
+@pytest.mark.unit
+@pytest.mark.high
+def test_load2():
+    """Test load function"""
+
+    with open(f"arquivo_indesejado-{uuid.uuid4()}.txt", "w") as file_:
+        file_.write("dados uteis somente para o teste")
+
     assert len(load(PEOPLE_FILE)) == 2
     assert load(PEOPLE_FILE)[0][0] == "J"
