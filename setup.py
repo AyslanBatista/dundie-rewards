27b92ccda4b5a4bf929f90b3f40be165ffa16d79
AyslanBatista <ayslan.batista@hotmail.com>
2024-04-02 23:49:42 -0300

    turned to installable binary #9

--- setup.py
+++ setup.py
@@ -6,4 +6,7 @@ setup(
     description="Reward Point System for Dunder Mifflin",
     author="Ayslan Batista",
     packages=find_packages(),
+    entry_points={
+        "console_scripts": ["dundie = dundie.__main__:main"]
+    },
 )
