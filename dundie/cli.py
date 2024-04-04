70f54478600284de05c4b7093502bb0dd62e3a5b
AyslanBatista <ayslan.batista@hotmail.com>
2024-04-03 21:07:05 -0300

    Organized tests

---
+++ dundie/cli.py
@@ -0,0 +1,29 @@
+import argparse
+
+from dundie.core import load
+
+
+# Função principal referente ao CLI
+def main():
+    parser = argparse.ArgumentParser(
+        description="Dunder Mifflin rewards CLI",
+        # Uma mensagem que será enviada no terminal para o usuario
+        epilog="Enjoy and use with cautious",
+    )
+    # Adicionando primeiro comando
+    parser.add_argument(
+        "subcommand",
+        type=str,  # Tipo de dado que ira receber no subcomando
+        help="The subcommand to run",
+        choices=("load", "show", "send"),  # comandos
+        default="help",
+    )
+    # Adicionando segundo comando
+    parser.add_argument(
+        "filepath", type=str, help="File path to load", default=None
+    )
+    args = parser.parse_args()
+
+    # linha globals()[args.subcommand](args.filepath) se tornará
+    # load('arquivo.txt'), assumindo que exista uma função chamada load
+    print(*globals()[args.subcommand](args.filepath))
---
+++ dundie/utils/log.py
@@ -0,0 +1,34 @@
+import logging
+import os
+from logging import handlers
+
+LOG_LEVEL = os.getenv("LOG_LEVEL", "WARNING").upper()
+log = logging.getLogger("dundie")  # Criando stancia de log
+# Objeto de formatação de como será exibido os logs
+fmt = logging.Formatter(
+    '%(asctime)s %(name)s %(levelname)s '
+    'l:%(lineno)d f:%(filename)s: %(message)s'
+)
+
+
+# Controle de logs do usuario pela variavel de ambiente
+# export LOG_LEVEL=debug
+# export serve para ativar o log level em formato DEV, para exibir os debug
+def get_logger(logfile="dundie.log"):
+    """Returns a configured logger."""
+
+    # Responsavel por salvar os log em arquivo
+    fh = handlers.RotatingFileHandler(
+        # Nome do arquivo
+        logfile,
+        # maxBytes = 10**6 Tamanho maximo do arquivo, depois cria outro arquivo
+        maxBytes=10**6,
+        # backupCount=10 >> Quantidade de arquivos para manter no backup
+        backupCount=10,
+    )
+
+    fh.setLevel(LOG_LEVEL)  # Nivel que será exibido
+
+    fh.setFormatter(fmt)  # Adicionando a formtação ao Handler que foi criado
+    log.addHandler(fh)  # Por fim adiciona o Handler ao log
+    return log
