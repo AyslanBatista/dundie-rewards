33065267de9db00eed6459eba98a94fce3fb3475
AyslanBatista <ayslan.batista@hotmail.com>
2024-04-04 15:54:52 -0300

    Adding finalziers, setup and teardown to tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import pytest
+
 MARKER = """\
 integration: Mark integration tests
 unit: Mark unit tests
@@ -10,3 +12,15 @@ low: low Priority
 def pytest_configure(config):
     for line in MARKER.split("\n"):
         config.addinivalue_line('markers', line)
+
+
+# Dados fixos - dados que temos para popular durante o test
+# autouse=True - Automaticamente todos os test do projeto irão usar a fixture
+@pytest.fixture(autouse=True)
+def go_to_tmpdir(request):  # mandar toda a "sujeira" para o tmp
+    # Quem fornece o request é o pytest | injeção de dependencias
+    tmpdir = request.getfixturevalue("tmpdir")  # Diretorio temporario
+    # With abaixo Muda o diretorio atual
+    # Ele ira rodar cada test dentro do contexto abaixo
+    with tmpdir.as_cwd():
+        yield  # protocolo de generators | para que cada função rode aqui
--- tests/constants.py
+++ tests/constants.py
@@ -1,1 +1,5 @@
-PEOPLE_FILE = "tests/assets/people.csv"
+import os
+
+# Pegando caminho completo de onde o arquivo se encontra
+TEST_PATH = os.path.dirname(__file__)
+PEOPLE_FILE = os.path.join(TEST_PATH, "assets/people.csv")
