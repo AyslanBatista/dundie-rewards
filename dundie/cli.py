a71903cb352260219ed989eb84e65145d1af3cbd
AyslanBatista <ayslan.batista@hotmail.com>
2024-04-05 19:04:02 -0300

    Implemented JSON database
    closes #1

--- dundie/cli.py
+++ dundie/cli.py
@@ -1,29 +1,53 @@
-import argparse
+import pkg_resources
+import rich_click as click
+from rich.console import Console
+from rich.table import Table
 
-from dundie.core import load  # noqa
+from dundie import core
+
+# Conjunto de configurações do Rich
+click.rich_click.USE_RICH_MARKUP = True
+click.rich_click.USE_MARKDOWN = True
+click.rich_click.SHOW_ARGUMENTS = True
+click.rich_click.GROUP_ARGUMENTS_OPTIONS = True
+click.rich_click.SHOW_METAVARS_COLUMN = False
+click.rich_click.APPEND_METAVARS_HELP = True
 
 
-# Função principal referente ao CLI
+@click.group()  # Responsavel pelos subcomandos
+@click.version_option(  # pegando a versão do programa, dundie --version
+    pkg_resources.get_distribution("dundie").version
+)
 def main():
-    parser = argparse.ArgumentParser(
-        description="Dunder Mifflin rewards CLI",
-        # Uma mensagem que será enviada no terminal para o usuario
-        epilog="Enjoy and use with cautious",
-    )
-    # Adicionando primeiro comando
-    parser.add_argument(
-        "subcommand",
-        type=str,  # Tipo de dado que ira receber no subcomando
-        help="The subcommand to run",
-        choices=("load", "show", "send"),  # comandos
-        default="help",
-    )
-    # Adicionando segundo comando
-    parser.add_argument(
-        "filepath", type=str, help="File path to load", default=None
-    )
-    args = parser.parse_args()
+    """Dunder Mifflin Rewards System.
 
-    # linha globals()[args.subcommand](args.filepath) se tornará
-    # load('arquivo.txt'), assumindo que exista uma função chamada load
-    print(*globals()[args.subcommand](args.filepath))
+    This cli application controls DM rewards.
+    """
+
+
+# Qualquer nome de função se tonar um subcomando para o cli
+@main.command()
+@click.argument("filepath", type=click.Path(exists=True))  # checkar argumento
+def load(filepath):  # injeção de dependencia
+    """Loads the file to the database
+
+        ## Features
+
+    - Validdates data
+    - Parses the file
+    - Load to database
+    """
+    table = Table(
+        title="Dunder Mifflin Associates", style="cyan", title_style="bold"
+    )
+    headers = ["name", "dept", "role", "created", "e-mail"]
+    for header in headers:
+        table.add_column(header, style="green")
+
+    result = core.load(filepath)
+    for person in result:
+        table.add_row(*[str(value) for value in person.values()])
+
+    # Console vai calcular o tamanho do terminal para exibição da tabela
+    console = Console()
+    console.print(table)
--- integration/test_load.py
+++ integration/test_load.py
@@ -1,18 +1,19 @@
-from subprocess import CalledProcessError, check_output
-
 import pytest
+from click.testing import CliRunner
+
+from dundie.cli import load, main
+
+from .constants import PEOPLE_FILE
+
+cmd = CliRunner()
 
 
 @pytest.mark.integration
 @pytest.mark.medium
 def test_load_positive_call_load_command():
     """test command load"""
-    out = (
-        check_output(["dundie", "load", "tests/assets/people.csv"])
-        .decode("utf-8")
-        .split("\n")
-    )
-    assert len(out) == 2
+    out = cmd.invoke(load, PEOPLE_FILE)  # Ele vai invocar a função para test
+    assert "Dunder Mifflin Associates" in out.output
 
 
 @pytest.mark.integration
@@ -23,10 +24,7 @@ def test_load_positive_call_load_command():
 )
 def test_load_negative_call_load_command_with_wrong_params(wrong_command):
     """test command load"""
-    with pytest.raises(CalledProcessError) as error:  # testando o erro
-        check_output(
-            check_output(["dundie", wrong_command, "tests/assets/people.csv"])
-            .decode("utf-8")
-            .split("\n")
-        )
-    assert "status 2" in str(error.getrepr())
+    out = cmd.invoke(main, wrong_command, PEOPLE_FILE)
+
+    assert out.exit_code != 0
+    assert f"No such command '{wrong_command}'." in out.output
